4474c8e84beb00017b88234190f1db75eb366e63
Matt Jones <matthew.s.jones@intel.com>
2025-02-05 15:07:40 -0800

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -1,28 +1,28 @@
-# Python script for linear regression 
+# Python script for simple linear regression 
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Feature vector
+# define feature vector
 x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
 
-# Response vector
+# define response vector
 y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12, 11, 13, 15, 15, 14, 16])
 
 def get_coef(x, y):
     
-    # Number of data points
+    # get number of data points
     n = np.size(x)
 
-    # Means of x and y 
+    # calculate means of x and y 
     m_x = np.mean(x)
     m_y = np.mean(y)
 
-    # Cross-deviation and deviation about x
+    # calculate cross-deviation and deviation about x
     SS_xy = np.sum(y*x) - n*m_y*m_x
     SS_xx = np.sum(x*x) - n*m_x*m_x
 
-    # Regression coefficients
+    # calculate regression coefficients
     b_1 = SS_xy / SS_xx
     b_0 = m_y - b_1*m_x
     
@@ -30,25 +30,27 @@ def get_coef(x, y):
     
 def plot_line(x, y, b_0, b_1):
     
-    # plotting the actual points as scatter plot
+    # plot the data points as scatter plot
     plt.scatter(x, y, color = "m", marker = "o", s = 30)
 
-    # predicted response vector
+    # create regression line
     y_pred = b_0 + b_1*x
 
-    # plotting the regression line
+    # plot the regression line
     plt.plot(x, y_pred, color = "g")
 
-    # putting labels
+    # add axis labels
     plt.xlabel('x')
     plt.ylabel('y')
-    
+
+    # show the plot    
     plt.show()
     
-# estimating coefficients
+# estimate coefficients
 (b_0, b_1) = get_coef(x, y)
 print("Coefficients:\nb_0 = {} \nb_1 = {}".format(b_0, b_1))
-    
+
+# plot data and regression line    
 plot_line(x, y, b_0, b_1)
 
 exit()
