bf3d0ebfe1b70d741dac35f752d41c1a3c40d403
Matt Jones <matthew.s.jones@intel.com>
2025-02-05 13:25:13 -0800

    Update linear_regression.py

--- linear_regression.py
+++ linear_regression.py
@@ -22,12 +22,13 @@ def get_coef(x, y):
     
     return (b_0, b_1)
     
-def plot_line(x, y, b):
+def plot_line(x, y, b_0, b_1):
+    
     # plotting the actual points as scatter plot
     plt.scatter(x, y, color = "m", marker = "o", s = 30)
 
     # predicted response vector
-    y_pred = b[0] + b[1]*x
+    y_pred = b_0 + b_1*x
 
     # plotting the regression line
     plt.plot(x, y_pred, color = "g")
@@ -36,15 +37,18 @@ def plot_line(x, y, b):
     plt.xlabel('x')
     plt.ylabel('y')
     
-def main():
+    plt.show()
     
-    # Feature vector
-    x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+# Feature vector
+x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
-    # Response vector
-    y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
+# Response vector
+y = np.array([1, 3, 2, 5, 7, 8, 8, 9, 10, 12])
 
-    # estimating coefficients
-    b = get_coef(x, y)
-    print("Coefficients:\nb_0 = {} \nb_1 = {}".format(b
+# estimating coefficients
+(b_0, b_1) = get_coef(x, y)
+print("Coefficients:\nb_0 = {} \nb_1 = {}".format(b_0, b_1))
     
+plot_line(x, y, b_0, b_1)
+
+exit()
